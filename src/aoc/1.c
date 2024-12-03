65dc34e5c643134e8e062a5b980d099999d574ea
Kyle Hodowany <kylehodowany@utexas.edu>
2024-12-03 09:29:27 -0600

    Remove utl_array

--- src/aoc/1.c
+++ src/aoc/1.c
@@ -1,4 +1,5 @@
 #include "aoc/1.h"
+#include "khg_utl/algorithm.h"
 #include "khg_utl/array.h"
 #include "khg_utl/file_reader.h"
 #include "khg_utl/map.h"
@@ -7,7 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 
-static int compare_ints_arr(const void* a, const void* b) {
+static int compare_ints(const void *a, const void *b) {
   return (*(int*)a - *(int*)b);
 }
 
@@ -21,9 +22,11 @@ void day0101(const char *input_path, const uint16_t num_lines) {
   printf("Advent of Code 2024: Day 1 Part 1\n");
   printf("Using file: %s\n", input_path);
   utl_file_reader *reader = utl_file_reader_open(input_path, UTL_READ_TEXT);
-  utl_array *arrs[2];
-  arrs[0] = utl_array_create(sizeof(int), num_lines);
-  arrs[1] = utl_array_create(sizeof(int), num_lines);
+  int *arrs[2];
+  int arr1[num_lines];
+  int arr2[num_lines];
+  arrs[0] = arr1;
+  arrs[1] = arr2;
   int count = 0;
   char buffer[256];
   while (utl_file_reader_read_line(buffer, sizeof(buffer), reader)) {
@@ -31,23 +34,22 @@ void day0101(const char *input_path, const uint16_t num_lines) {
     char *token;
     token = strtok(buffer, delimiter);
     int num_count = 0;
-    while (token != NULL) {
+    while (token) {
       int num = atoi(token);
-      utl_array_set(arrs[num_count], count, &num);
+      arrs[num_count][count] = num;
       token = strtok(NULL, delimiter);
       num_count++;
     }
     count++;
   }
-  utl_array_sort(arrs[0], compare_ints_arr);
-  utl_array_sort(arrs[1], compare_ints_arr);
+  utl_algorithm_sort(arr1, num_lines, sizeof(int), compare_ints);
+  utl_algorithm_sort(arr2, num_lines, sizeof(int), compare_ints);
   int total_distance = 0;
   for (int i = 0; i < num_lines; i++) {
-    total_distance += abs(*(int *)utl_array_at(arrs[0], i) - *(int *)utl_array_at(arrs[1], i));
+    printf("%i, %i\n", arrs[0][i], arrs[1][i]);
+    total_distance += abs(arrs[0][i] - arrs[1][i]);
   }
   printf("Answer: %i\n", total_distance);
-  utl_array_deallocate(arrs[0]);
-  utl_array_deallocate(arrs[1]);
   utl_file_reader_close(reader);
 };
 
@@ -55,9 +57,11 @@ void day0102(const char *input_path, const uint16_t num_lines) {
   printf("Advent of Code 2024: Day 1 Part 2\n");
   printf("Using file: %s\n", input_path);
   utl_file_reader* reader = utl_file_reader_open(input_path, UTL_READ_TEXT);
-  utl_array *arrs[2];
-  arrs[0] = utl_array_create(sizeof(int), num_lines);
-  arrs[1] = utl_array_create(sizeof(int), num_lines);
+  int *arrs[2];
+  int arr1[num_lines];
+  int arr2[num_lines];
+  arrs[0] = arr1;
+  arrs[1] = arr2;
   int count = 0;
   char buffer[256];
   while (utl_file_reader_read_line(buffer, sizeof(buffer), reader)) {
@@ -65,9 +69,9 @@ void day0102(const char *input_path, const uint16_t num_lines) {
     char *token;
     token = strtok(buffer, delimiter);
     int num_count = 0;
-    while (token != NULL) {
+    while (token) {
       int num = atoi(token);
-      utl_array_set(arrs[num_count], count, &num);
+      arrs[num_count][count] = num;
       token = strtok(NULL, delimiter);
       num_count++;
     }
@@ -75,7 +79,7 @@ void day0102(const char *input_path, const uint16_t num_lines) {
   }
   utl_map *freq_table = utl_map_create(compare_ints_map, free, free);
   for (int i = 0; i < num_lines; i++) {
-    int num = *(int *)utl_array_at(arrs[1], i);
+    int num = arrs[1][i];
     int *val = utl_map_at(freq_table, &num);
     if (val == NULL) {
       int *idx = malloc(sizeof(int));
@@ -90,7 +94,7 @@ void day0102(const char *input_path, const uint16_t num_lines) {
   }
   int similarity_score = 0;
   for (int i = 0; i < num_lines; i++) {
-    int num = *(int *)utl_array_at(arrs[0], i);
+    int num = arrs[0][i];
     int *freq = (int *)utl_map_at(freq_table, &num);
     if (!freq) {
       continue;
@@ -99,8 +103,6 @@ void day0102(const char *input_path, const uint16_t num_lines) {
   }
   printf("Answer: %i\n", similarity_score);
   utl_map_deallocate(freq_table);
-  utl_array_deallocate(arrs[0]);
-  utl_array_deallocate(arrs[1]);
   utl_file_reader_close(reader);
 };
 
